b3f3353f13bdca9abf7a07dbc9d17c58cc8f8aaf
Oleg Schegol <olschegolev@yahoo.com>
2023-12-05 21:14:53 +0500

    start

---
+++ src/test/java/com/example/pizzaweb/PizzaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
